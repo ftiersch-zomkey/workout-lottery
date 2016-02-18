7b5cb1ce51e6c47a9a712f619a20128d44b593ee
ftiersch-zomkey <mail@ftiersch.de>
2016-02-18 20:38:53 +0100

    - started migration

--- database/migrations/2016_02_18_191343_initial_creation.php
+++ database/migrations/2016_02_18_191343_initial_creation.php
@@ -12,7 +12,50 @@ class InitialCreation extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+
+            $table->smallInteger('interval_minutes', false, true);
+
+            $table->nullableTimestamps();
+        });
+
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name');
+
+            $table->nullableTimestamps();
+        });
+
+        Schema::create('groups_users', function (Blueprint $table) {
+            $table->integer('group_id', false, true);
+            $table->integer('user_id', false, true);
+
+            $table->nullableTimestamps();
+
+            $table->primary(['group_id', 'user_id']);
+
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('groups_exercises', function (Blueprint $table) {
+            $table->integer('group_id', false, true);
+            $table->integer('user_id', false, true);
+
+            $table->integer('min_reps', false, true)->default(1);
+            $table->integer('max_reps', false, true)->default(1);
+
+            $table->nullableTimestamps();
+
+            $table->primary(['group_id', 'user_id']);
+
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
     }
 
     /**
