d0e6e88fdd67e94896fcf03b328571d291d9cf34
ftiersch-zomkey <mail@ftiersch.de>
2016-02-18 20:56:21 +0100

    - started migration

--- database/migrations/2016_02_18_191343_initial_creation.php
+++ database/migrations/2016_02_18_191343_initial_creation.php
@@ -17,7 +17,14 @@ class InitialCreation extends Migration
 
             $table->string('name');
 
-            $table->smallInteger('interval_minutes', false, true);
+            $table->smallInteger('interval_minutes', false, true)->default(30);
+            $table->time('interval_time_start', false, true)->default('10:00:00');
+            $table->time('interval_time_end', false, true)->default('20:00:00');
+
+            $table->smallInteger('number_of_winners', false, true)->default(1);
+            $table->tinyInteger('number_of_winners_is_percent', false, true)->default(0);
+
+            $table->smallInteger('finish_excercise_time')->default(25);
 
             $table->nullableTimestamps();
         });
@@ -26,6 +33,7 @@ class InitialCreation extends Migration
             $table->increments('id');
 
             $table->string('name');
+            $table->text('description')->nullable();
 
             $table->nullableTimestamps();
         });
@@ -54,6 +62,30 @@ class InitialCreation extends Migration
             $table->primary(['group_id', 'user_id']);
 
             $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('draws', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('group_id', false, true);
+
+            $table->nullableTimestamps();
+
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
+        });
+
+        Schema::create('draws_users', function (Blueprint $table) {
+            $table->integer('draw_id', false, true);
+            $table->integer('user_id', false, true);
+
+            $table->tinyInteger('succeeded', false, true)->default(0);
+
+            $table->nullableTimestamps();
+
+            $table->primary(['draw_id', 'user_id']);
+
+            $table->foreign('draw_id')->references('id')->on('draws')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
