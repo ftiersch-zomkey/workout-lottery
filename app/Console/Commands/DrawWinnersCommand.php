2e576093a7b151cc58b3291f342c6416e37faf72
Frank Tiersch Extern <ftiersch.extern@chip.de>
2016-02-22 11:26:42 +0100

    - added command to draw winners

---
+++ app/Console/Commands/DrawWinnersCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Group;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class DrawWinnersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'winners:draw';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Get\'s called every minute to draw the winners of each group';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $currentTime = Carbon::now();
+        $groupsToDraw = Group:: where('interval_time_start', '<=', $currentTime->format('H:i:s'))
+                                ->where('interval_time_end', '>=', $currentTime->format('H:i:s'))
+                                ->whereExists(function ($query) {
+                                    $query  ->select(DB::raw(1))
+                                        ->from('draws')
+                                        ->where('draws.group_id = groups.id')
+                                        ->whereRaw('draws.created_at < DATE_SUB(NOW(), INTERVAL groups.interval_minutes MINUTE)');
+                                })
+                                ->get();
+
+        if (!$groupsToDraw->isEmpty()) {
+            foreach ($groupsToDraw as $group) {
+
+            }
+        }
+        // find groups that need to be drawn
+        // - current time is in group's "active period"
+        // - last draw has been longer than the interval
+        // cycle through groups and draw Winners
+        // notify winners depending on group type
+    }
+}
